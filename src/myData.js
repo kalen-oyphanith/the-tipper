82cd6464553bbb7b94a1520fd39cda780605d605
Kalen Oyphanith <kmo6884@g.rit.edu>
2020-10-04 21:19:51 -0400

    client html and css done

--- src/myData.js
+++ src/myData.js
@@ -1,102 +1,110 @@
 const reviews = {};
 
 const respondJSON = (request, response, status, object) => {
-  const headers = {
-    'Content-Type': 'application/json',
-  };
-  response.writeHead(status, headers);
-  response.write(JSON.stringify(object));
-  response.end();
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    response.writeHead(status, headers);
+    response.write(JSON.stringify(object));
+    response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-  const headers = {
-    'Content-Type': 'application/json',
-  };
+    const headers = {
+        'Content-Type': 'application/json',
+    };
     // send response without json object, just headers
-  response.writeHead(status, headers);
-  response.end();
+    response.writeHead(status, headers);
+    response.end();
 };
 
 const getTip = (request, response) => {
-  const responseJSON = {
-    message: 'This is your bill with tip.',
-  };
+    const responseJSON = {
+        message: 'Tipped!',
+    };
 
-  return respondJSON(request, response, 200, responseJSON);
+    return respondJSON(request, response, 200, responseJSON);
 };
 
 const getTipMeta = (request, response) => respondJSON(request, response, 200);
 
 const getReviews = (request, response) => {
-  const responseJSON = {
-    reviews,
-  };
+    const responseJSON = {
+        reviews,
+    };
 
-  return respondJSON(request, response, 200, responseJSON);
+    return respondJSON(request, response, 200, responseJSON);
 };
 
-const getReviewsMeta = (request, response) => {
-  respondJSONMeta(request, response, 200);
-};
+const getReviewsMeta = (request, response) => respondJSONMeta(request, response, 200);
+
 
 const addReview = (request, response, body) => {
-  const responseJSON = {
-    message: 'The location, rating, and description are both required.',
-  };
+    const responseJSON = {
+        message: 'The location, rating, description, and date are all required.',
+    };
 
-  if (!body.name || !body.description || !body.rating || !body.date) {
-    responseJSON.id = 'missingParams';
-    return respondJSON(request, response, 400, responseJSON);
-  }
+    if (!body.name || !body.description || !body.rating || !body.date) {
+        responseJSON.id = 'missingParams';
+        return respondJSON(request, response, 400, responseJSON);
+    }
 
-  let responseCode = 201; // created
+    let responseCode = 201; // created
 
-  if (reviews[body.name]) {
-    responseCode = 204; // check to see if user exists
-  } else {
-    // otherwise create an object with that name
-    reviews[body.name] = {};
-    reviews[body.name].name = body.name;
-  }
-  reviews[body.name].description = body.description;
-  reviews[body.name].rating = body.rating;
-  reviews[body.name].date = body.date;
+    if (reviews[body.name]) {
+        responseCode = 204; // check to see if user exists
+    } else {
+        // otherwise create an object with that name
+        reviews[body.name] = {};
+        reviews[body.name].name = body.name;
+    }
+    reviews[body.name].description = body.description;
+    reviews[body.name].rating = body.rating;
+    reviews[body.name].date = body.date;
 
-  if (responseCode === 201) {
-    responseJSON.message = 'review was created';
-    return respondJSON(request, response, responseCode, responseJSON);
-  }
-  // no body
-  return respondJSONMeta(request, response, responseCode);
+    if (responseCode === 201) {
+        responseJSON.message = 'review was created';
+        return respondJSON(request, response, responseCode, responseJSON);
+    }
+    // no body
+    return respondJSONMeta(request, response, responseCode);
 };
 
 const deleteReview = (request, response) => {
-  const responseJSON = {
-    message: 'Review was deleted!',
-  };
+    const responseJSON = {
+        message: 'Reviews deleted!',
+        reviews
+    };
+    for (let review in reviews) {
+        delete reviews[review];
+        console.log(reviews);
+        console.log(review);
+    }
 
-  return respondJSON(request, response, 200, responseJSON);
+    return respondJSON(request, response, 200, responseJSON);
+
 };
 
-const notFound = (request, response) => {
-  const responseJSON = {
-    message: 'The page you are looking for was not found.',
-    id: 'notFound',
-  };
+const deletReviewMeta = () => respondJSONMeta(request, response, 202);
 
-  return respondJSON(request, response, 404, responseJSON);
+const notFound = (request, response) => {
+    const responseJSON = {
+        message: 'The page you are looking for was not found.',
+        id: 'notFound',
+    };
+
+    return respondJSON(request, response, 404, responseJSON);
 };
 
 const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
 
 module.exports = {
-  getTip,
-  getTipMeta,
-  deleteReview,
-  getReviewsMeta,
-  getReviews,
-  addReview,
-  notFound,
-  notFoundMeta,
+    getTip,
+    getTipMeta,
+    deleteReview,
+    getReviewsMeta,
+    getReviews,
+    addReview,
+    notFound,
+    notFoundMeta,
 };
--- src/server.js
+++ src/server.js
@@ -8,64 +8,61 @@ const myData = require('./myData.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  GET: {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    '/getReviews': myData.getReviews,
-    '/getTip': myData.getTip,
-    '/deleteReview': myData.deleteReview,
-    notFound: myData.notFound,
-  },
-  HEAD: {
-    '/getReviews': myData.getReviewsMeta,
-    '/getTipMeta': myData.getTipMeta,
-    notFound: myData.notFoundMeta,
-  },
-  DELETE: {
-    '/deleteReview': myData.deleteReview,
-    notFound: myData.notFound,
-  },
+    GET: {
+        '/': htmlHandler.getIndex,
+        '/style.css': htmlHandler.getCSS,
+        '/getReviews': myData.getReviews,
+        '/getTip': myData.getTip,
+        '/deleteReview': myData.deleteReview,
+        notFound: myData.notFound,
+    },
+    HEAD: {
+        '/getReviews': myData.getReviewsMeta,
+        '/getTipMeta': myData.getTipMeta,
+        notFound: myData.notFoundMeta,
+    },
+    DELETE: {
+        '/deleteReview': myData.deleteReview,
+        '/deleteReviewMeta': myData.deleteReviewMeta,
+        notFound: myData.notFound,
+    },
 };
 
 const handlePost = (request, response, parsedUrl) => {
-  if (parsedUrl.pathname === '/addReview') {
-    const body = [];
+    if (parsedUrl.pathname === '/addReview') {
+        const body = [];
 
-    request.on('error', (err) => {
-      console.dir(err);
-      response.statusCode = 400;
-      response.end();
-    });
+        request.on('error', (err) => {
+            console.dir(err);
+            response.statusCode = 400;
+            response.end();
+        });
 
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    });
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
 
-    request.on('end', () => {
-      const bodyString = Buffer.concat(body).toString();
-      const bodyParams = query.parse(bodyString);
-      myData.addReview(request, response, bodyParams);
-      console.log(bodyParams);
-    });
-  }
+        request.on('end', () => {
+            const bodyString = Buffer.concat(body).toString();
+            const bodyParams = query.parse(bodyString);
+            myData.addReview(request, response, bodyParams);
+        });
+    }
 };
 
 const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
+    const parsedUrl = url.parse(request.url);
 
-  console.dir(parsedUrl.pathname);
-  console.dir(request.method);
-
-  if (request.method === 'POST') {
-    handlePost(request, response, parsedUrl);
-    console.log(parsedUrl);
-  } else if (request.method === 'GET' || request.method === 'HEAD' || request.method === 'DELETE') {
-    if (urlStruct[request.method][parsedUrl.pathname]) {
-      urlStruct[request.method][parsedUrl.pathname](request, response);
-    } else {
-      urlStruct[request.method].notFound(request, response);
+    if (request.method === 'POST') {
+        handlePost(request, response, parsedUrl);
+        //console.log(parsedUrl);
+    } else if (request.method === 'GET' || request.method === 'HEAD' || request.method === 'DELETE') {
+        if (urlStruct[request.method][parsedUrl.pathname]) {
+            urlStruct[request.method][parsedUrl.pathname](request, response);
+        } else {
+            urlStruct[request.method].notFound(request, response);
+        }
     }
-  }
 };
 
 http.createServer(onRequest).listen(port);
